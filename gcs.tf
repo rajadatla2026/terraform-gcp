216b9d32667b010a4daae8ed017fbe31d373331e
rajadatla2026 <rajadatla2026@gmail.com>
2026-08-05 18:31:20 +0530

    changed bucket name

--- gcs.tf
+++ gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "gcs_bucket" {
-  name          = "bucket-for-terraform"
+  name          = "rajasekhar-terraform-bucket"
   location      = "US"
   storage_class = "STANDARD"
 
