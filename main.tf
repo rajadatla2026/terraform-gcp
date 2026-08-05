9ede1a1fd246a2ddbaf29b36a1c9bc9cbf42af09
rajadatla2026 <rajadatla2026@gmail.com>
2026-08-05 16:40:45 +0530

    vpc creation

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "google_service_account" "terraform_gcp_sa" {
   display_name = "sample-sa-account"
   description  = "Service Account used by Terraform deployments"
 }
-/*resource "google_project_service" "compute_api" {
+resource "google_project_service" "compute_api" {
   project = var.project_id
   service = "compute.googleapis.com"
 
@@ -37,5 +37,5 @@ resource "google_compute_subnetwork" "private_subnet" {
   network       = google_compute_network.vpc.id
 
   private_ip_google_access = true
-}*/
+}
 
